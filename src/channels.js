13b3db4f08a3a6882055a32a75e5589b325b9afe
Jixuan Yu <z5248177@ad.unsw.edu.au>
2023-02-21 23:16:44 +1100

    iteration 0

---
+++ src/channels.js
@@ -0,0 +1,27 @@
+function channelsCreateV1( authUserId, name, isPublic ) {
+    return { channelId: 1 }
+
+}
+
+function channelsListV1 ( authUserId ) {
+    return {
+        channels: [
+          {
+            channelId: 1,
+            name: 'My Channel',
+          }
+        ],
+      }
+
+}
+
+function channelsListAllV1( authUserId, channelId ) {
+    return {
+        channels: [
+          {
+            channelId: 1,
+            name: 'My Channel',
+          }
+        ],
+      } 
+}
