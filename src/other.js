572a972cba1fddd80557834e50d0ecfcfce8c742
Kevin Sutandi <k.sutandi@student.unsw.edu.au>
2023-03-08 19:18:08 +1100

    Update other.js as clearV1 was not final

--- src/other.js
+++ src/other.js
@@ -1,10 +1,1 @@
-import { getData, setData } from "./dataStore";
 
-export function clearV1() {
-  const data = getData();
-  data.users = [];
-  data.channels = [];
-  setData(data);
-
-  return {};
-}
