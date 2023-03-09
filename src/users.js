2aa471dc56d380588a0631478f2d4c5f93855838
Almina Kovacevic <z5206142@ad.unsw.edu.au>
2023-03-09 14:18:44 +1100

    update code to match

--- src/users.js
+++ src/users.js
@@ -1,30 +1,31 @@
-import { getData, setData } from "./dataStore"
+import { getData, setData } from "./dataStore";
 
 // check if authUserId exists
 export function isUser(userId) {
-    const data = getData();
-    return data.users.some(a => a.authUserId === userId);
+  const data = getData();
+  return data.users.some((a) => a.authUserId === userId);
+}
+
+export function userProfileV1(authUserId, uId) {
+  const data = getData();
+  // function to check user is valid and correct
+  if (!isUser(authUserId)) {
+    console.log(authUserId);
+    return { error: "Invalid authUserId" };
+  }
+  // need to get a thing to check zid and make sure it is valid
+  if (!isUser(uId)) {
+    console.log(uId);
+    return { error: "Invalid uId" };
   }
 
-export function userProfileV1( authUserId, uId ) {
-    // function to check user is valid and correct
-    if (!isUser(authUserId)) {
-        console.log(authUserId);
-        return { error: 'Invalid authUserId' }
-    }
-    // need to get a thing to check zid and make sure it is valid
-    if (!isUser(uId)) {
-        console.log(uId);
-        return { error: 'Invalid uId' }
-    }
-    // if both are valid, we return information about the user accessing from our data
-    return { 
-        users: data.users.map(a => ({
-            uId: a.uId,
-            email: a.email,
-            nameFirst: a.nameFirst,
-            nameLast: a.nameLast,
-            handleStr: a.handleStr,  
-        })) 
-    }
+  const userNum = data.users.findIndex((a) => a.authUserId === uId);
+  // if both are valid, we return information about the user accessing from our data
+  return {
+    authUserId: data.users[userNum].authId,
+    authemail: data.users[userNum].authemail,
+    authfirstname: data.users[userNum].authfirstname,
+    authlastname: data.users[userNum].authlastname,
+    handlestring: data.users[userNum].handlestring,
+  };
 }
