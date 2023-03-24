913ca63a57bbc97e8bc65f9534353d182db932d2
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-03-24 18:29:17 +1100

    Implement requestHelper function for wrapping functions

---
+++ src/helper.ts
@@ -0,0 +1,19 @@
+import request, { HttpVerb } from 'sync-request';
+
+import { port, url } from './config.json';
+
+const SERVER_URL = `${url}:${port}`;
+const ERROR = { error: expect.any(String) };
+
+function requestHelper(method: HttpVerb, path: string, payload: object) {
+  let qs = {};
+  let json = {};
+  if (['GET', 'DELETE'].includes(method)) {
+    qs = payload;
+  } else {
+    // PUT/POST
+    json = payload;
+  }
+  const res = request(method, SERVER_URL + path, { qs, json, timeout: 20000 });
+  return JSON.parse(res.getBody('utf-8'));
+}
