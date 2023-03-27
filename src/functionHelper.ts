76e64d7bb722b61303c075dbf88f3c16bb8069b9
Kevin Sutandi <z5352065@ad.unsw.edu.au>
2023-03-27 13:14:18 +1100

    Added the function and along with helpers function should wokr

--- src/functionHelper.ts
+++ src/functionHelper.ts
@@ -111,3 +111,11 @@ export function findChannelByMessageId(messageId: number) {
   );
   return channelFound;
 }
+
+export function getAllOwnerIds(channel: channelData) {
+  if (channel) {
+    return channel.ownerMembers.map((owner) => owner.uId);
+  } else {
+    return null;
+  }
+}
