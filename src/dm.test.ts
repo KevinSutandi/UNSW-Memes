7a0b8ece36a36dc041bd185f497667ccc4d2e7f3
Almina Kovacevic <z5206142@ad.unsw.edu.au>
2023-03-28 20:21:34 +1100

    added the testing functions for dm create

---
+++ src/dm.test.ts
@@ -0,0 +1,52 @@
+import {
+    authRegister,
+    clearV1,
+    messageSend,
+    dmCreate,
+  } from './httpHelper';
+  import { AuthReturn } from './interfaces';
+  
+  const ERROR = { error: expect.any(String) };
+  
+  
+  // error testing
+  
+  describe('testing dm create v1', () => {
+  
+  
+  // error testing
+    test('any uId does not refer to a valid user', () => {
+      const user1 = authRegister(
+        'alminak1938@gmail.com',
+        'mina001',
+        'Almina',
+        'Kov',
+      );
+      expect(
+        messageSend(user1.token, user1.uId)
+      ).toStrictEqual(ERROR);
+      )
+    
+    test('user token is not valid', () => {
+        expect(
+          messageSend('alminaaaaascnj', user1.uId)
+        ).toStrictEqual(ERROR);
+      }); 
+  
+  
+  // test that dm is successful
+  
+  
+  
+  // test that if multiple are created that it is also successful
+  
+  
+  
+  
+  // test if one user valid but second dm is invalid
+  
+  
+  
+  
+  
+  });
