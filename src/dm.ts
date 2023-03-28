ccc4439ed7c8d32753a1ca806d66c7adc17d5502
Almina Kovacevic <z5206142@ad.unsw.edu.au>
2023-03-29 00:48:56 +1100

    added code for dmDetailsV1, still need to do testing

--- src/dm.ts
+++ src/dm.ts
@@ -1,3 +1,65 @@
-export function dmDetailsV1 () {
-    
+import { getData, setData } from './dataStore';
+import {
+  isUser,
+  findUser,
+  getUserByToken,
+} from './functionHelper';
+import { errorMessage } from './interfaces';
+
+export function isDm(dmId: number): boolean {
+    const data = getData();
+    return data.dm.some((a) => a.dmId === dmId);
+}
+
+export function isDmMember(dmId: number, userId: number): boolean {
+    const dm = findDm(dmId);
+    console.log(dmId);
+    const allMemberIds = getAllMemberIds(dm);
+    console.log(allMemberIds);
+    return allMemberIds.includes(userId);
+}
+  
+export function findDm(dmId: number): dmData | undefined {
+    const data = getData();
+    return data.dms.find((a) => a.dmId === dmId);
+}
+
+export function getAllMemberIds(dm: dmData) {
+    if (dm) {
+      return dm.allMembers.map((member) => member.uId);
+    } else {
+      return null;
+    }
+}
+
+export function findDm(dmId: number): dmData | undefined {
+    const data = getData();
+    return data.dm.find((a) => a.dmId === dmId);
+  }
+  
+  
+export function dmDetailsV1 (
+    token: string,
+    dmId: number,
+) {
+    const data = getData();
+    const user = getUserByToken(token);
+
+    if (user === undefined) {
+        return { error: 'Invalid token' };
+    }
+    if (!isDm(dmId)) {
+        return { error: 'dmId does not refer to a valid DM' };
+    }
+    if (!isDmMember(dmId, user.authUserId)) {
+        return { error: user.authUserId + ' is not a member of the DM'}
+    }
+
+    const dmDetsFinal = findDm(dmId);
+    return {
+        name: dmDetsFinal.name,
+        ownerMemebers: dmDetsFinal.ownerMemebers,
+        getAllMembers: dmDetsFinal.getAllMembers,
+    };
+
 }
