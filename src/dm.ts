5b9ad5cc1cda61d6964f6efc6ee50ffbdab7c160
Almina Kovacevic <z5206142@ad.unsw.edu.au>
2023-03-29 01:13:56 +1100

    added code for dmMessagesV1, need to go through and clean it up, but seems to do what is needed

--- src/dm.ts
+++ src/dm.ts
@@ -1,3 +1,81 @@
-export function dmMessagesV1() {
-    return
+import { getData, setData } from './dataStore';
+import { isUser, getUserByToken } from './functionHelper';
+import {
+  errorMessage,
+} from './interfaces.js';
+
+
+// helper functions
+
+export function isDm(dmId: number): boolean {
+    const data = getData();
+    return data.dm.some((a) => a.dmId === dmId);
+}
+
+export function isDmMember(dmId: number, userId: number): boolean {
+    const dm = findDm(dmId);
+    console.log(dmId);
+    const allMemberIds = getAllMemberIds(dm);
+    console.log(allMemberIds);
+    return allMemberIds.includes(userId);
+}
+  
+export function findDm(dmId: number): dmData | undefined {
+    const data = getData();
+    return data.dms.find((a) => a.dmId === dmId);
+}
+
+export function getAllMemberIds(dm: dmData) {
+    if (dm) {
+      return dm.allMembers.map((member) => member.uId);
+    } else {
+      return null;
+    }
+}
+
+export function findDm(dmId: number): dmData | undefined {
+    const data = getData();
+    return data.dm.find((a) => a.dmId === dmId);
+}
+
+
+// main function
+
+export function dmMessagesV1(
+    token: string,
+    dmId: number,
+    start: number,
+) {
+    const user = getUserByToken(token);
+
+    if (user === undefined) {
+        return { error: 'Invalid token' };
+    }
+    if (!isDm(dmId)) {
+        return { error: 'dmId does not refer to a valid DM' };
+    }
+    if (!isDmMember(dmId, user.authUserId)) {
+        return { error: user.authUserId + ' is not a member of the DM'}
+    }
+    
+    const dmMessages = dm.messages.length;
+    const uId = user.authUserId;
+
+    if (start > dmMessages) {
+        return { error: "'start' is greater than the amount of messages"}
+    }
+    if (start + 50 > dmMessages) {
+        return {
+            messages: dm.messages.slice(start),
+            start: start,
+            end: -1,
+        };
+    } else {
+        return {
+            messages: dm.messages.slice(start, start + 50),
+            start: start,
+            end: start + 50,
+        };
+    }
+
 }
