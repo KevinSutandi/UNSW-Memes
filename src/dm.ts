0b591a5767f6460593f8b5592411cda0e772bb8a
Almina Kovacevic <z5206142@ad.unsw.edu.au>
2023-03-28 23:55:45 +1100

    added some code for dmListV1, have to update interface

--- src/dm.ts
+++ src/dm.ts
@@ -1,3 +1,34 @@
-export function dmList(
-    
-);
+import { getData, setData } from './dataStore';
+import { isUser, getUserByToken } from './functionHelper';
+import {
+  errorMessage,
+} from './interfaces.js';
+
+export function dmListV1(
+    token: string
+): dmListReturn | errorMessage {
+
+    const data = getData();
+    const user = getUserByToken(token);
+
+    // errors
+    if (user === undefined) {
+        return { error: 'Invalid token' };
+    }
+
+    const userDms: dmListReturn = { dms: [] };
+
+
+    data.dms.forEach((dm) => {
+    const isUserInDm = dm.allMembers.some(
+      (member) => member.uId === authUserIdToFind
+    );
+    if (isUserInDm === true) {
+      userDm.dm.push({
+        dmId: dm.dmId,
+        name: dm.name,
+      });
+    }
+  });
+  return userDms;
+}
