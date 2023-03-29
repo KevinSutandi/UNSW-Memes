fb1c4989cf59b4aa7530be0233a3aef58d126375
Kevin Sutandi <z5352065@ad.unsw.edu.au>
2023-03-29 14:32:08 +1100

    Added tests for messageEdit

--- src/message.test.ts
+++ src/message.test.ts
@@ -5,6 +5,7 @@ import {
   clearV1,
   messageSend,
   messageRemove,
+  messageEdit,
 } from './httpHelper';
 import { AuthReturn } from './interfaces';
 
@@ -186,10 +187,10 @@ describe('testing removeMessages', () => {
   });
   test('remove other user message when user is channelOwner', () => {
     const user2 = authRegister(
-      'kevins050324@gmail.com',
-      'kevin1001',
-      'Kevin',
-      'Sutandi'
+      'kevinesutandi@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'Bongo'
     );
     const user3 = authRegister(
       'welovedogs@gmail.com'
@@ -204,24 +205,175 @@ describe('testing removeMessages', () => {
   });
   test('remove other user message when user is channelMember', () => {
     const user2 = authRegister(
-      'kevins050324@gmail.com',
-      'kevin1001',
-      'Kevin',
-      'Sutandi'
+      'kevinesutandi@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'Bongo'
     );
     channelJoin(user2.token, channel1.channelId);
     expect(messageRemove(user2.token, message1.messageId)).toStrictEqual(ERROR);
   });
   test('remove own message while not channelOwner', () => {
     const user2 = authRegister(
-      'kevins050324@gmail.com',
-      'kevin1001',
-      'Kevin',
-      'Sutandi'
+      'kevinesutandi@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'Bongo'
     );
     channelJoin(user2.token, channel1.channelId);
     const message2 = messageSend(user2.token, channel1.channelId, 'test moments');
     expect(messageRemove(user2.token, message2.messageId)).toStrictEqual({});
   });
    */
+});
+
+describe('testing messageEdit', () => {
+  let user1: AuthReturn;
+  let channel1: { channelId: number };
+  let message1: { messageId: number };
+  beforeEach(() => {
+    clearV1();
+    user1 = authRegister(
+      'kevins050324@gmail.com',
+      'kevin1001',
+      'Kevin',
+      'Sutandi'
+    );
+    channel1 = channelsCreate(user1.token, 'wego', true);
+    message1 = messageSend(user1.token, channel1.channelId, 'test moments');
+  });
+
+  afterEach(() => {
+    clearV1();
+  });
+
+  test('token is invalid', () => {
+    expect(
+      messageEdit('invalidtoken', message1.messageId, 'hello')
+    ).toStrictEqual(ERROR);
+  });
+
+  test('user is not in channel', () => {
+    const user2 = authRegister(
+      'kevinesutandi@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'Bongo'
+    );
+    expect(messageEdit(user2.token, message1.messageId, 'hello')).toStrictEqual(
+      ERROR
+    );
+  });
+
+  test('message does not exist', () => {
+    expect(
+      messageEdit(user1.token, message1.messageId + 200, 'hello')
+    ).toStrictEqual(ERROR);
+  });
+
+  test('message length over 1000', () => {
+    expect(
+      messageEdit(user1.token, message1.messageId, 'a'.repeat(1001))
+    ).toStrictEqual(ERROR);
+  });
+
+  test('delete message when message string empty', () => {
+    expect(messageEdit(user1.token, message1.messageId, '')).toStrictEqual({});
+    expect(channelMessage(user1.token, channel1.channelId, 0)).toStrictEqual({
+      messages: [],
+      start: 0,
+      end: -1,
+    });
+  });
+
+  test('cannot edit message after deleting', () => {
+    expect(messageEdit(user1.token, message1.messageId, '')).toStrictEqual({});
+    expect(
+      messageEdit(user1.token, message1.messageId, 'hello world')
+    ).toStrictEqual(ERROR);
+  });
+
+  test('edit own message', () => {
+    expect(
+      messageEdit(user1.token, message1.messageId, 'hello world')
+    ).toStrictEqual({});
+    expect(channelMessage(user1.token, channel1.channelId, 0)).toStrictEqual({
+      messages: [
+        {
+          messageId: message1.messageId,
+          uId: user1.authUserId,
+          message: 'hello world',
+          timeSent: expect.any(Number),
+        },
+      ],
+      start: 0,
+      end: -1,
+    });
+  });
+
+  /**
+   *  Will enable tests when channelJoin is available to test multiple user in channel
+   */
+
+  /*
+  test('edit other user message when user is globalOwner in channel', () => {
+    const user2 = authRegister(
+      'kevinesutandi@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'Bongo'
+    );
+    const channel2 = channelsCreate(user2.token, 'wego', true);
+    channelJoin(user1.token, channel2.channelId);
+    const message2 = messageSend(user2.token, channel2.channelId, 'test moments');
+    expect(
+      messageEdit(user1.token, message2.messageId, 'hello world')
+    ).toStrictEqual({});
+    expect(channelMessage(user1.token, channel2.channelId, 0)).toStrictEqual({
+      messages: [
+        {
+          messageId: message2.messageId,
+          uId: user2.authUserId,
+          message: 'hello world',
+          timeSent: expect.any(Number),
+        }
+      ],
+      start: 0,
+      end: -1,
+    });
+  });
+  test('edit other user message when user is channelOwner', () => {
+    const user2 = authRegister(
+      'kevinesutandi@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'Bongo'
+    );
+    const user3 = authRegister(
+      'burneremail304@gmail.com',
+      'lesgo1001',
+      'Bevin',
+      'DONGO'
+    );
+    const channel2 = channelsCreate(user2.token, 'wego', true);
+    channelJoin(user3.token, channel2.channelId);
+    const message3 = messageSend(user3.token, channel2.channelId, 'test moments');
+    expect(
+      messageEdit(user2.token, message3.messageId, 'hello world')
+    ).toStrictEqual({});
+    expect(channelMessage(user2.token, channel2.channelId, 0)).toStrictEqual({
+      messages: [
+        {
+          messageId: message3.messageId,
+          uId: user3.authUserId,
+          message: 'hello world',
+          timeSent: expect.any(Number),
+        }
+      ],
+      start: 0,
+      end: -1,
+    });
+  });
+
+  */
 });
