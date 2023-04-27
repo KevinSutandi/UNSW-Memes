1f72728de368dbd9a3fcdd1deb16e5d3fc5d0d01
Kevin Sutandi <z5352065@ad.unsw.edu.au>
2023-04-27 15:31:53 +1000

    Fixed server.ts not using localhost

--- src/server.ts
+++ src/server.ts
@@ -78,7 +78,7 @@ app.use(morgan('dev'));
 app.use('/img', express.static('img'));
 
 const PORT: number = parseInt(process.env.PORT || config.port);
-const HOST: string = process.env.IP || config.url;
+const HOST: string = process.env.IP || 'localhost';
 
 // Example Route
 app.get('/echo', (req: Request, res: Response, next) => {
